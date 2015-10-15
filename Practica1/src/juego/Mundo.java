e688ab524cab22786f8dda8a916fa32d9cff67b1
Espartaco99 <jperucha@ucm.es>
2015-10-15 14:19:10 +0200

    Añadidos crear y eliminar celulaSuperficie

--- Practica1/src/juego/Mundo.java
+++ Practica1/src/juego/Mundo.java
@@ -4,6 +4,29 @@ public class Mundo {
 	private Superficie superficie;
 	
 	
+	private static final int NUMEROCELULAS = 6;
+	private static final int NUMEROFILAS = 3;
+	private static final int NUMEROCOLUMNAS = 4;
+	 	
+		
+	/**
+	 * La clase constructor de Mundo, genera una superficie con una longitud
+	 * especifica de filas y columnas, y genera NUMEROCELULAS en posiciones
+	 * aleatorias para esa superficie.
+	 */
+	public Mundo(int f, int c, int n){
+		this.superficie = new Superficie(f, c);
+		for (int i = 0; i < n; i++){
+			generarCelulaAleatoria();
+		}
+	}
+	//Lo dejo de para pregunta cual de los 2 es mejor
+	/*
+	public Mundo(int n){
+		this.superficie = new Superficie(NUMEROFILAS, NUMEROCOLUMNAS);
+		for (int n = 0; n < NUMEROCELULAS; n++){
+		generarCelulaAleatoria();
+
 	
 /**
  * 
@@ -11,50 +34,70 @@ public class Mundo {
  * @param nc valor entero positivo que indica el numero de columnas de la superficie
  * @param n numero entero positivo de celulas que se generan en la superficie
  */
-	public Mundo(int nf, int nc, int n){
-		this.superficie = new Superficie(nf, nc);
-		for (int i = 0; i < n; i++){
-			generarCelulaAleatoria();
-		}
-	}
 	/**
-	 * para cada célula de la superficie ejecuta un paso de acuerdo a las
-		reglas que se describieron en la Sección 2. El método evoluciona recorre la superficie
-		y dependiendo de las reglas de la vida, va pidiéndola a ésta que realice los pasos
-		pertinentes. Ten en cuenta que este método es el que determina cómo se mueven las
-		células, las coloca, comprueba si mueren o se reproducen, etc.. Para ello tendrá que
-		dar las órdenes pertienentes a la superficie para poder consultar la información sobre
-		las células. Por otro lado cuando el mundo evoluciona, hay que controlar que una
-		célula no se mueva dos veces en el mismo paso de evolución.
+	 * para cada cÃ©lula de la superficie ejecuta un paso de acuerdo a las
+		reglas que se describieron en la SecciÃ³n 2. El mÃ©todo evoluciona recorre la superficie
+		y dependiendo de las reglas de la vida, va pidiÃ©ndola a Ã©sta que realice los pasos
+		pertinentes. Ten en cuenta que este mÃ©todo es el que determina cÃ³mo se mueven las
+		cÃ©lulas, las coloca, comprueba si mueren o se reproducen, etc.. Para ello tendrÃ¡ que
+		dar las Ã³rdenes pertienentes a la superficie para poder consultar la informaciÃ³n sobre
+		las cÃ©lulas. Por otro lado cuando el mundo evoluciona, hay que controlar que una
+		cÃ©lula no se mueva dos veces en el mismo paso de evoluciÃ³n.
 	 */
 	public void evoluciona(){
 		
 	}
 	
 	/**
-	 * 
-	 * @param f
-	 * @param c
-	 * @return
-	 */	  
+	 * Crea una celula en la posicion (f,c) de la superficie
+	 * @param f Valor entero positivo fila de la matriz
+	 * @param c Valor entero positivo columna de la matriz
+	 * @return Si se ha hecho el proceso de crear la celula
+	 */
 	public boolean crearCelulaSuperficie(int f, int c){
-		return true;
+		boolean ok = false;
+		if (f < NUMEROFILAS && c < NUMEROCOLUMNAS && f >= 0 && c >= 0){
+			superficie.crear(f, c);
+			ok = true;
+		}
+		return ok;
 	}
 	
 	/**
-	 * 
-	 * @param f
-	 * @param c
-	 * @return
-	 */	 
+	 * Elimina la celula en la posicion (f,c) de la superficie
+	 * @param f Valor entero positivo fila de la matriz
+	 * @param c Valor entero positivo columna de la matriz
+	 * @return Si se ha hecho el proceso de eliminar la celula
+	 */
 	public boolean eliminarCelulaSuperficie(int f, int c){
-		return true;
+		boolean ok = false;
+		if (f < NUMEROFILAS && c < NUMEROCOLUMNAS && f >= 0 && c >= 0){
+			superficie.borrar(f, c);
+			ok = true;
+		}
+		return ok;
 	}
 	
 	/**
-	 * Generar la celulas en la superficie de forma random
+	 * Mirar como funciona la funcion
+	 * @param n valor entero positivo
+	 * @return un valor entero positivo a modulo de n
+	 */
+		private static int generaPosicion(int n){
+			return (int)(Math.floor(Math.random()*101) % n);
+		}
+
+	
+	
+	/**
+	 * Genera una celula en la superficie de forma random
 	 */
 	public void generarCelulaAleatoria(){
+		int f, c;
+		//AÃ±adir el metodo aleatorio
+		superficie.crear(f, c);
 		
 	}
+
 }
+

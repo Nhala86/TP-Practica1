9caf16e8041015b02008507192218795d8a19bed
Espartaco99 <jperucha@ucm.es>
2015-10-18 22:04:06 +0200

    Rellenado el main

--- Practica1/src/juego/Main.java
+++ Practica1/src/juego/Main.java
@@ -1,11 +1,15 @@
 package juego;
 
+import java.util.Scanner;
+
 public class Main {
-
 	
+	//NO SE PROBARLO PARA VER SI FUNCIONA EL TABLERO Y LO DEMAS
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		Mundo mundo = new Mundo();
+		Controlador controlador = new Controlador(mundo, in);
+		controlador.realizaSimulacion();
 	}
 
 }

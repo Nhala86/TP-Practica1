3fb4bc71ed08b19817762553ae2e9b2b943df05e
Espartaco99 <jperucha@ucm.es>
2015-10-18 22:00:00 +0200

    Añadidos comentarios

--- Practica1/src/juego/Celula.java
+++ Practica1/src/juego/Celula.java
@@ -4,7 +4,7 @@ public class Celula {
 	private int noMovimiento;
 	private int movimiento;
 	
-// Crea una celula con movimiento = 0 y con numero de movimientos antes de morir = 0
+	// Crea una celula con movimiento = 0 y con numero de movimientos antes de morir = 0
 	public Celula(){
 		this.noMovimiento = 1;
 		this.movimiento = 0;
@@ -14,7 +14,11 @@ public class Celula {
 		this.noMovimiento = noMovimiento;
 		this.movimiento = movimiento;
 	}
-
+	
+	/**
+     * Coge los valores numericos de noMovimiento y movimiento y los convierte en una cadena para mostrar
+     * @return Una cadena con los dos parametros numericos de la celula, separados por un guion
+     */
     public String toString(){
        return this.noMovimiento + "-" + this.movimiento;
     }
